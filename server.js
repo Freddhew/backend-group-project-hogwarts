56310181addeb9c69958e22b6de89b34dcb4965f
Freddhew <fredrik_wisborg@hotmail.com>
2022-03-30 13:11:25 +0200

    Project structuring

--- server.js
+++ server.js
@@ -5,20 +5,16 @@ const path = require("path");
 const routerApply =       require("./Api/Apply-Api");
 const routerCourses =     require("./Api/Courses-Api");
 const routerEducation =   require("./Api/Education-Api");
-const routerLogin =      require("./Api/Login-Api");
+const routerLogin =       require("./Api/Login-Api");
 const routerStaff =       require("./Api/Staff-Api");
 
 const app = express();
-const port = 8080;
+const port = 5000;
+const handleStaticFiles = express.static(path.join(__dirname, "..", "group-project-client", "build"));
 
 app.use(bodyParser.json());
 app.use(handleStaticFiles);
 
-const handleStaticFiles = express.static(
-  path.join(__dirname, "..", "client", "build")
-);
-
-app.use("", routes);
 app.use(routerApply);
 app.use(routerCourses);
 app.use(routerEducation);
