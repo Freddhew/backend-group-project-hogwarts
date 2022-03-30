8269cc2361aed522d0bfbe8e2078ea3b15e7e28f
Freddhew <fredrik_wisborg@hotmail.com>
2022-03-30 12:34:57 +0200

    Preparation for individual work

--- server.js
+++ server.js
@@ -2,27 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const routes = require("./coursesApiRoutes");
-const routerApply = require("./apiApply");
-const routerLoggin = require("./loggin");
+const routerApply =       require("./Api/Apply-Api");
+const routerCourses =     require("./Api/Courses-Api");
+const routerEducation =   require("./Api/Education-Api");
+const routerLogin =      require("./Api/Login-Api");
+const routerStaff =       require("./Api/Staff-Api");
 
 const app = express();
 const port = 8080;
 
 app.use(bodyParser.json());
+app.use(handleStaticFiles);
 
 const handleStaticFiles = express.static(
   path.join(__dirname, "..", "client", "build")
 );
-app.use(handleStaticFiles);
-app.use("", routes);
-const router = require("./apiStaff");
-const educationApi = require("./apiEducation");
 
-app.use(educationApi);
+app.use("", routes);
 app.use(routerApply);
-app.use(routerLoggin);
-app.use(router);
+app.use(routerCourses);
+app.use(routerEducation);
+app.use(routerLogin);
+app.use(routerStaff);
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
