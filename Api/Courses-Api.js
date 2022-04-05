69fea404ef82a101494ff88f447669f849acdf55
Freddhew <fredrik_wisborg@hotmail.com>
2022-04-05 12:26:41 +0200

    Backend data update to courses content

--- Api/Courses-Api.js
+++ Api/Courses-Api.js
@@ -6,7 +6,7 @@ let courses = [
   // Pre added data
   {
     courseId: 9876543,
-    courseName: "Application of dark arts",
+    courseName: "Application of Dark Arts",
     teacher: "Professor Severus Snape",
     courseLength: "12 weeks",
     courseDescription: `"The Dark Arts are many, varied, ever-changing, and eternal. Fighting them is like fighting a many-headed monster, which, each time a neck is severed, sprouts a head even fiercer and cleverer than before. You are fighting that which is unfixed, mutating, indestructible."
@@ -15,9 +15,9 @@ let courses = [
   {
     courseId: 1356789,
     courseName: "Defence Against the Dark Arts",
-    teacher: "Ruth",
+    teacher: "Ruth Ravendale",
     courseLength: "12 weeks",
-    courseDescription: `And then, once you had managed to find them, there were the classes themselves. There was a lot more to magic, as Harry quickly found out, than waving your wand and saying a few funny words."
+    courseDescription: `Defend yourself. Survive. Forget your last course about learning the dark arts. Unlearn it. That is how we defeat this problem. There was a lot more to magic, as Harry quickly found out, than waving your wand and saying a few funny words."
     — The challenging nature of academic classes at Hogwarts`
   },
   {
@@ -25,14 +25,14 @@ let courses = [
     courseName: "History of Magic",
     teacher: "Ruth Ravendale",
     courseLength: "8 weeks",
-    courseDescription: `...of the exhibition, a once-in-a-lifetime collaboration between Bloomsbury, J.K. Rowling and the brilliant curators of the British Library. It promises to take readers on a fascinating journey through the subjects studied at Hogwarts School of Witchcraft and Wizardry...`
+    courseDescription: `Learning the magics history is the key to Great exhibition, a once-in-a-lifetime collaboration between Bloomsbury, J.K. Rowling and the brilliant curators of the British Library. It promises to take readers on a fascinating journey through the subjects studied at Hogwarts School of Witchcraft and Wizardry...`
   },
   {
     courseId: 12566,
     courseName: "Transfiguration",
     teacher: "Ruth Ravendale",
     courseLength: "12 weeks",
-    courseDescription: `"I do hope they start right away, there's so much to learn, I'm particularly interested in Transfiguration, you know, turning something into something else, of course, it's supposed to be very difficult —"
+    courseDescription: `Learn to throw frogs! Explode frogs! Multiply frogs. "I do hope they start right away, there's so much to learn, I'm particularly interested in Transfiguration, you know, turning something into something else, of course, it's supposed to be very difficult —"
     — Description of Transfiguration`
   },
   {
@@ -48,7 +48,7 @@ let courses = [
     courseName: "Poison",
     teacher: "Professor Severus Snape",
     courseLength: "12 weeks",
-    courseDescription: `"Ron had dropped his glass; he half-rose from his chair and then crumpled, his extremities jerking uncontrollably. Foam was dribbling from his mouth and his eyes were bulging from their sockets."
+    courseDescription: `Poison is an affect that can cause good and harm. Learning poison and its usage is essential in becoming a wizard. "Ron had dropped his glass; he half-rose from his chair and then crumpled, his extremities jerking uncontrollably. Foam was dribbling from his mouth and his eyes were bulging from their sockets."
     — Ron Weasley after having consumed poisoned oak matured mead`
   }
 ];
