777a15b64f109afd384d6475d8c666b180123094
Freddhew <fredrik_wisborg@hotmail.com>
2022-04-08 13:12:00 +0200

    Backend Teacher naming update

--- Api/Courses-Api.js
+++ Api/Courses-Api.js
@@ -7,7 +7,7 @@ let courses = [
   {
     courseId: 9876543,
     courseName: "Application of Dark Arts",
-    teacher: "Professor Severus Snape",
+    teacher: "Rebeus Hagrig",
     courseLength: "12 weeks",
     courseDescription: `"The Dark Arts are many, varied, ever-changing, and eternal. Fighting them is like fighting a many-headed monster, which, each time a neck is severed, sprouts a head even fiercer and cleverer than before. You are fighting that which is unfixed, mutating, indestructible."
     — Severus Snape's description of the Dark Arts`
@@ -15,7 +15,7 @@ let courses = [
   {
     courseId: 1356789,
     courseName: "Defence Against the Dark Arts",
-    teacher: "Ruth Ravendale",
+    teacher: "Minerva Mcgonagall",
     courseLength: "12 weeks",
     courseDescription: `Defend yourself. Survive. Forget your last course about learning the dark arts. Unlearn it. That is how we defeat this problem. There was a lot more to magic, as Harry quickly found out, than waving your wand and saying a few funny words."
     — The challenging nature of academic classes at Hogwarts`
@@ -23,14 +23,14 @@ let courses = [
   {
     courseId: 126264,
     courseName: "History of Magic",
-    teacher: "Ruth Ravendale",
+    teacher: "Minerva Mcgonagall",
     courseLength: "8 weeks",
     courseDescription: `Learning the magics history is the key to Great exhibition, a once-in-a-lifetime collaboration between Bloomsbury, J.K. Rowling and the brilliant curators of the British Library. It promises to take readers on a fascinating journey through the subjects studied at Hogwarts School of Witchcraft and Wizardry...`
   },
   {
     courseId: 12566,
     courseName: "Transfiguration",
-    teacher: "Ruth Ravendale",
+    teacher: "Draco Lucius Malfoy",
     courseLength: "12 weeks",
     courseDescription: `Learn to throw frogs! Explode frogs! Multiply frogs. "I do hope they start right away, there's so much to learn, I'm particularly interested in Transfiguration, you know, turning something into something else, of course, it's supposed to be very difficult —"
     — Description of Transfiguration`
@@ -38,7 +38,7 @@ let courses = [
   {
     courseId: 75322,
     courseName: "Charms",
-    teacher: "Ruth Ravendale",
+    teacher: "Draco Lucius Malfoy",
     courseLength: "8 weeks",
     courseDescription: `"Charms differ from Transfiguring Spells in the following manner: a charm adds certain properties to an object or creature, whereas a transfiguring spell will change it into something utterly different."
     — The Standard Book of Spells, Grade 1`
@@ -46,16 +46,11 @@ let courses = [
   {
     courseId: 1266743,
     courseName: "Poison",
-    teacher: "Professor Severus Snape",
+    teacher: "Alexander Winqvist",
     courseLength: "12 weeks",
     courseDescription: `Poison is an affect that can cause good and harm. Learning poison and its usage is essential in becoming a wizard. "Ron had dropped his glass; he half-rose from his chair and then crumpled, his extremities jerking uncontrollably. Foam was dribbling from his mouth and his eyes were bulging from their sockets."
     — Ron Weasley after having consumed poisoned oak matured mead`
   }
-];
-
-let teachers = [
-  "Ruth Ravendale",
-  "Professor Severus Snape"
 ];
 
 routes.get("/Courses", (req, res) => {
