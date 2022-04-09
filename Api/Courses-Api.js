4829cb38ff5e0e484f9b939204ff250aef5e008d
Freddhew <fredrik_wisborg@hotmail.com>
2022-04-09 18:10:10 +0200

    Finishing up final code

--- Api/Courses-Api.js
+++ Api/Courses-Api.js
@@ -8,7 +8,7 @@ let courses = [
     courseId: 9876543,
     courseName: "Application of Dark Arts",
     teacher: "Rebeus Hagrig",
-    courseLength: "12 weeks",
+    courseDuration: "12 weeks",
     courseDescription: `"The Dark Arts are many, varied, ever-changing, and eternal. Fighting them is like fighting a many-headed monster, which, each time a neck is severed, sprouts a head even fiercer and cleverer than before. You are fighting that which is unfixed, mutating, indestructible."
     — Severus Snape's description of the Dark Arts`
   },
@@ -16,7 +16,7 @@ let courses = [
     courseId: 1356789,
     courseName: "Defence Against the Dark Arts",
     teacher: "Minerva Mcgonagall",
-    courseLength: "12 weeks",
+    courseDuration: "12 weeks",
     courseDescription: `Defend yourself. Survive. Forget your last course about learning the dark arts. Unlearn it. That is how we defeat this problem. There was a lot more to magic, as Harry quickly found out, than waving your wand and saying a few funny words."
     — The challenging nature of academic classes at Hogwarts`
   },
@@ -24,14 +24,14 @@ let courses = [
     courseId: 126264,
     courseName: "History of Magic",
     teacher: "Minerva Mcgonagall",
-    courseLength: "8 weeks",
+    courseDuration: "8 weeks",
     courseDescription: `Learning the magics history is the key to Great exhibition, a once-in-a-lifetime collaboration between Bloomsbury, J.K. Rowling and the brilliant curators of the British Library. It promises to take readers on a fascinating journey through the subjects studied at Hogwarts School of Witchcraft and Wizardry...`
   },
   {
     courseId: 12566,
     courseName: "Transfiguration",
     teacher: "Draco Lucius Malfoy",
-    courseLength: "12 weeks",
+    courseDuration: "12 weeks",
     courseDescription: `Learn to throw frogs! Explode frogs! Multiply frogs. "I do hope they start right away, there's so much to learn, I'm particularly interested in Transfiguration, you know, turning something into something else, of course, it's supposed to be very difficult —"
     — Description of Transfiguration`
   },
@@ -39,7 +39,7 @@ let courses = [
     courseId: 75322,
     courseName: "Charms",
     teacher: "Draco Lucius Malfoy",
-    courseLength: "8 weeks",
+    courseDuration: "8 weeks",
     courseDescription: `"Charms differ from Transfiguring Spells in the following manner: a charm adds certain properties to an object or creature, whereas a transfiguring spell will change it into something utterly different."
     — The Standard Book of Spells, Grade 1`
   },
@@ -47,7 +47,7 @@ let courses = [
     courseId: 1266743,
     courseName: "Poison",
     teacher: "Alexander Winqvist",
-    courseLength: "12 weeks",
+    courseDuration: "12 weeks",
     courseDescription: `Poison is an affect that can cause good and harm. Learning poison and its usage is essential in becoming a wizard. "Ron had dropped his glass; he half-rose from his chair and then crumpled, his extremities jerking uncontrollably. Foam was dribbling from his mouth and his eyes were bulging from their sockets."
     — Ron Weasley after having consumed poisoned oak matured mead`
   }
@@ -75,7 +75,7 @@ routes.post("/Courses", (req, res) => {
     courseId: req.body.id,
     courseName: req.body.courseName,
     teacher: req.body.teacher,
-    courseLength: req.body.courseLength,
+    courseDuration: req.body.courseDuration,
     courseDescription: req.body.courseDescription,
   };
 
@@ -92,14 +92,14 @@ routes.put("/Courses/:courseId", (req, res) => {
   const courseId = Number(req.params.courseId);
   const courseName = req.body.courseName;
   const teacher = req.body.teacher;
-  const courseLength = req.body.courseLength;
+  const courseDuration = req.body.courseDuration;
   const courseDescription = req.body.courseDescription;
 
   const newCourse = {
     courseId: courseId,
     courseName: courseName,
     teacher: teacher,
-    courseLength: courseLength,
+    courseDuration: courseDuration,
     courseDescription: courseDescription,
   };
 
